d7a70348723c16012872b3a0f0d2baf0767c1995
aliJo <ali.jomehri@chapagha.com>
2022-11-19 16:48:08 +0330

    docker vs multiple mysql databases

--- docker/provision/mysql/init/databases.sql
+++ docker/provision/mysql/init/databases.sql
@@ -3,5 +3,7 @@ CREATE DATABASE IF NOT EXISTS `qst_db`;
 CREATE DATABASE IF NOT EXISTS `qst_testing_db`;
 
 # create root user and grant rights
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+
+FLUSH PRIVILEGES;
