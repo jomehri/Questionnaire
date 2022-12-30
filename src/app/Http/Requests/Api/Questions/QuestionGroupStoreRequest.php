27912b18ead6c504c666ff44eb18ed7bbb4566a9
AliJomehri <ajomehri@gmail.com>
2022-12-30 13:02:47 +0330

    question groups

--- src/app/Http/Requests/Api/Questions/QuestionGroupStoreRequest.php
+++ src/app/Http/Requests/Api/Questions/QuestionGroupStoreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Api\Questions;
 
 use App\Models\BaseModel;
+use App\Models\Questions\Questioner;
 use Illuminate\Validation\Rule;
 use App\Http\Requests\Api\BaseRequest;
 use App\Models\Questions\QuestionGroup;
@@ -29,6 +30,8 @@ class QuestionGroupStoreRequest extends BaseRequest
         return [
             QuestionGroup::COLUMN_TITLE => ['required', 'string', 'max:250',
                 Rule::unique(QuestionGroup::getDBTable())->whereNull(BaseModel::COLUMN_DELETED_AT)],
+            'questioner_ids' => ['nullable', 'array'],
+            'questioner_ids.*' => ['exists:questioners,id'],
         ];
     }
 
@@ -41,6 +44,8 @@ class QuestionGroupStoreRequest extends BaseRequest
             QuestionGroup::COLUMN_TITLE . '.required' => __("questions/question_group.validations.titleIsRequired"),
             QuestionGroup::COLUMN_TITLE . '.max' => __("questions/question_group.validations.titleIsTooLong"),
             QuestionGroup::COLUMN_TITLE . '.unique' => __("questions/question_group.validations.titleAlreadyExists"),
+            'questioner_ids' . '.array' => __("questions/question_group.validations.questionerIdsMustBeArray"),
+            'questioner_ids.*' . '.exists' => __("questions/question_group.validations.questionerIdNotFound"),
         ];
     }
 
--- src/resources/lang/persian/questions/question_group.php
+++ src/resources/lang/persian/questions/question_group.php
@@ -7,7 +7,9 @@ return [
         'titleIsRequired' => 'وارد کردن عنوان پرسشنامه الزامی است.',
         'titleIsTooLong' => 'عنوان پرسشنامه بیش از حد طولانی است.',
         'titleAlreadyExists' => 'پرسشنامه ای با این عنوان قبلا ثبت شده است.',
-        
+        'questionerIdsMustBeArray' => 'آرایه شماره پرسشنامه ها به شکل صحیح اسال نشده است.',
+        'questionerIdNotFound' => 'شماره پرسشنامه ارسال شده :attribute یافت نشد.',
+
     ],
 
     'exceptions' => [
