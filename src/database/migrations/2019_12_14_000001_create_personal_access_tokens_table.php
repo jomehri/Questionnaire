58892bf414a29d345322fa39af9464cb9a92e5ac
Ali <ajomehri@gmail.com>
2023-01-01 12:31:55 +0330

    migration fix

--- src/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ src/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -23,14 +23,12 @@ class CreatePersonalAccessTokensTable extends BaseMigration
      */
     protected function createTable(Blueprint $table): void
     {
-        $table->bigIncrements('id');
         $table->morphs('tokenable');
         $table->string('name');
         $table->string('token', 64)->unique();
         $table->text('abilities')->nullable();
         $table->timestamp('last_used_at')->nullable();
         $table->timestamp('expires_at')->nullable();
-        $table->timestamps();
     }
 
     /**
