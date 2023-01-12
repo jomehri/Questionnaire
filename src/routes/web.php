0660ff1debe5de07a74c56a3bea5307bedbbadaa
Ali <ajomehri@gmail.com>
2023-01-12 23:42:53 +0330

    swagger admin tags

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Ybazli\Faker\Facades\Faker;
 
 Route::get('/', function () {
-    return "up and working, checkout the <a href='" . env("APP_URL") . "/swagger' target='_blank'>swagger</a> documentation";
+    return "up and working, checkout the <a href='" . env("APP_URL") . "/swagger' target='_blank'>swagger</a> documentation" .
+        " or <a href='" . env("APP_URL") . "/horizon-management' target='_blank'>horizon</a>";
 });
