1cf4573918d7bbd6ac916bfc9e4128a37c17f7c4
Ali <ajomehri@gmail.com>
2023-01-12 12:47:15 +0330

    bash files

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 cd docker &&
 docker-compose up -d &&
 docker-compose exec qst-php-web php artisan migrate:rollback &&
-docker-compose exec qst-php-web php artisan migrate
+docker-compose exec qst-php-web php artisan migrate &&
+docker-compose exec qst-php-web php artisan db:seed &&
+docker-compose exec qst-php-web php artisan cache:clear &&
+docker-compose exec qst-php-web php artisan storage:link &&
+docker-compose exec qst-php-web php artisan l5-swagger:generate
