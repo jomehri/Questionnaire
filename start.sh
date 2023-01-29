e96d8bb7e475d8d73e7d751f38b15cbb804bc1e4
Ali <ajomehri@gmail.com>
2023-01-29 16:24:08 +0330

    bash

--- start.sh
+++ start.sh
@@ -1,10 +1,25 @@
-cd docker &&
-docker-compose up -d &&
-docker-compose exec qst-php-web php artisan migrate:rollback &&
-docker-compose exec qst-php-web php artisan migrate &&
-docker-compose exec qst-php-web php artisan db:seed &&
-docker-compose exec qst-php-web php artisan cache:clear &&
-rm -Rf ../src/public/storage &&
-docker-compose exec qst-php-web php artisan storage:link &&
-docker-compose exec qst-php-web chmod 777 storage/ -R &&
-docker-compose exec qst-php-web php artisan l5-swagger:generate
+cd src && source .env;
+
+APP_ENV=$(echo "$APP_ENV");
+echo "Deploying $APP_ENV";
+
+if [ "$APP_ENV" == "local" ]
+then
+   cd ../docker &&
+   docker-compose up -d &&
+   docker-compose exec qst-php-web php artisan migrate:rollback &&
+   docker-compose exec qst-php-web php artisan migrate &&
+   docker-compose exec qst-php-web php artisan db:seed &&
+   docker-compose exec qst-php-web php artisan cache:clear &&
+   rm -Rf ../src/public/storage &&
+   docker-compose exec qst-php-web php artisan storage:link &&
+   docker-compose exec qst-php-web chmod 777 storage/ -R &&
+   docker-compose exec qst-php-web php artisan l5-swagger:generate
+else
+    git stash &&
+    git pull origin main &&
+    cd src &&
+    php artisan migrate &&
+    php artisan db:seed &&
+    php artisan cache:clear
+fi
--- stop.sh
+++ stop.sh
@@ -1,2 +1,12 @@
-cd docker &&
-docker-compose down
+cd src && source .env;
+
+APP_ENV=$(echo "$APP_ENV");
+echo "Deploying $APP_ENV";
+
+if [ "$APP_ENV" == "local" ]
+then
+   cd ../docker &&
+   docker-compose down;
+else
+    echo 'To pause production do it on Arvan panel';
+fi
